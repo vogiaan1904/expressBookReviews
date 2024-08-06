df69614cb1f07fadf780ad83acc8757b7290b793
vogiaan1904 <vogiaan1904@gmail.com>
2024-08-06 18:56:26 +0700

    get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,8 +29,11 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author
+  const booksByAuthor = Object.values(books).filter((book)=>{
+    return book.author === author
+  })
+  return res.send(JSON.stringify(booksByAuthor,null,4))
 });
 
 // Get all books based on title
